4d86eed0d010c9911815cc9f83b4effb519849b8
Chelsea Quigley <20096536@mail.wit.ie>
2024-03-09 13:04:47 +0000

    Stored reviews in the context

--- movies/src/contexts/moviesContext.js
+++ movies/src/contexts/moviesContext.js
@@ -19,12 +19,14 @@ const MoviesContextProvider = (props) => {
   };
   
   
+  
   // We will use this function in a later section
   const removeFromFavorites = (movie) => {
     setFavorites( favorites.filter(
       (mId) => mId !== movie.id
     ) )
   };
+
 
   const addReview = (movie, review) => {
     setMyReviews( {...myReviews, [movie.id]: review } )
@@ -33,15 +35,15 @@ const MoviesContextProvider = (props) => {
 
   return (
     <MoviesContext.Provider
-      value={{
-        favorites,
-        addToFavorites,
-        removeFromFavorites,
-        addReview,
-      }}
-    >
-      {props.children}
-    </MoviesContext.Provider>
+    value={{
+      favorites,
+      addToFavorites,
+      removeFromFavorites,
+      addReview,
+    }}
+  >
+    {props.children}
+  </MoviesContext.Provider>
   );
 };
 
